9a6d8533195233f2926d7fbcdec728eb3978a60d
darmiel <71837281+darmiel@users.noreply.github.com>
2021-02-03 11:01:30 +0100

    Initial commit

---
+++ helloworld.s
@@ -0,0 +1,32 @@
+; --------------------------------------------- ;
+;                 hello world			;
+;						;
+; build & run (linux, i386):			;
+;						;
+; $ nasm -f elf32 -o helloworld.o helloworld.s	;
+; $ ld -m elf_i386 -o helloworld helloworld.o	;
+; $ ./helloworld				;
+; --------------------------------------------- ;
+
+global _start
+
+section .helloworld:
+_start:
+	; $ man 2 write
+	; ssize_t write(int fd, const void *buf, size_t count);
+	mov eax, 0x4
+	mov ebx, 0x1 	; fd
+	mov ecx, str_hw	; buf
+	mov edx, len_hw	; count
+	int 0x80
+
+	; $ man 2 exit
+	; void _exit(int status);
+	mov eax, 0x1
+	mov ebx, 0x0	; status
+	int 0x80
+
+section .txt:
+	str_hw: db "Hello World! :)", 0xA
+	len_hw: equ $-str_helloworld
+
